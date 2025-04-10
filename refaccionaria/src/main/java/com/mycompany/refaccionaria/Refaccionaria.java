2e95f0032ec6a9b7e690a1b419f0819fa6031d2b
rodri <rodri@192.168.56.1>
2025-04-09 19:56:43 -0700

    cree el proyecto

---
+++ refaccionaria/src/main/java/com/mycompany/refaccionaria/Refaccionaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.refaccionaria;
+
+/**
+ *
+ * @author rodri
+ */
+public class Refaccionaria {
+
+    public static void main(String[] args) {
+        System.out.println("esto es una prueba !");
+    }
+}
